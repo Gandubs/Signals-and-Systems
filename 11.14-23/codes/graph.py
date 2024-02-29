eecc5c666919779241db75f6425ec5d3c5ce5a99
h.ananthakrishnan <h.ananthakrishnan2004@gmail.com>
2024-02-29 21:09:30 +0530

    ec-48

--- 11.14-23/codes/graph.py
+++ 11.14-23/codes/graph.py
@@ -12,17 +12,18 @@ plt.figure(figsize=(8, 6))
 line, = plt.plot(t, theta, color='blue')  
 plt.axhline(y=max_value, color='gray', linestyle='--')  
 plt.axhline(y=min_value, color='red', linestyle='--')  
-plt.text(0.1, max_value * 1.05, r'$\left(\frac{I}{\alpha}\right)\theta^\prime(0)$', fontsize=8, color='blue')
-plt.text(0.1, min_value * 0.95, r'$\left(\frac{I}{\alpha}\right)\theta^\prime(0)$', fontsize=8, color='red')
+plt.text(0.1, max_value * 1.05, r'$\sqrt{\frac{I}{\alpha}}\theta^\prime(0)$', fontsize=12, color='blue')
+plt.text(0.1, min_value * 0.95, r'$\sqrt{\frac{I}{\alpha}}\theta^\prime(0)$', fontsize=12, color='red')
 plt.xlabel('t')
 plt.ylabel('θ(t)')
 yticks = np.arange(min_value, max_value * 1.2, max_value * 0.25)
-format_label = np.vectorize(lambda val: '{:.2f}($\\left(\\frac{{I}}{{\\alpha}}\\right)\\Theta\'(0)$)'.format(val), otypes=[str])
+format_label = np.vectorize(lambda val: '({:.2f})'.format(val), otypes=[str])
 yticklabels = format_label(yticks)
+yticklabels = ['($\\sqrt{\\frac{I}{\\alpha}}$)' + label + '$\\theta\'(0)$' for label in yticklabels]
 plt.yticks(yticks, yticklabels, fontsize=8)
 plt.grid(True)
 
-plt.legend([line], [r'$\left(\frac{I}{\alpha}\right)\theta^\prime(0) \, \sin(4.187t)$'], loc='upper right', bbox_to_anchor=(1, 1.02), fontsize=8)
+plt.legend([line], [r'$\sqrt{\frac{I}{\alpha}}\theta^\prime(0) \, \sin(4.187t)$'], loc='upper right', bbox_to_anchor=(1, 1.02), fontsize=8)
 
 plt.show()
 
